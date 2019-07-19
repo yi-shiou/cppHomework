406d4b1ed208e5f19348b0dbc91ca9d8d6a3ee8c
yi-shiou <hn88714013@hotmail.com>
2019-07-19 17:07:33 +0800

    pointer problem

---
+++ List.cpp
@@ -0,0 +1,93 @@
+﻿
+#include <iostream>
+struct List
+{
+    int val;
+    List* next = NULL;
+};
+
+List* del(List* l, int i) {
+    List* current = l;
+    int index = 0;
+    if (l == NULL) return l;
+    if (i == 0) {
+        l = current->next;
+        std::cout << current << " ";
+        delete current;
+        return l;
+    }
+    while (index < i - 1 && current != NULL) {
+        index++;
+        current = current->next;
+    }//index == i - 1;
+    List* swap = current->next->next;
+    delete current->next;
+    current->next = swap;
+    return l;
+}
+
+void del2(List* l, int i) {
+    List* current = l;
+    int index = 0;
+    if (l == NULL) return;
+    if (i == 0) {
+        l = current->next;//**************** 要怎摸改?
+        std::cout << current << " ";
+        delete current;
+        return;
+    }
+    while (index < i - 1 && current != NULL) {
+        index++;
+        current = current->next;
+    }//index == i - 1;
+    List* swap = current->next->next;
+    delete current->next;
+    current->next = swap;
+}
+
+int main()
+{
+    List* l = new List;
+    List* current;
+    int nums[] = { 1,2,3,4,5 };
+
+    //add array to list
+    l->val = nums[0];
+    current = l;
+    for (int i = 1; i < sizeof(nums)/sizeof(nums[0]); i++)
+    {
+        if (current->next == NULL) {
+            current->next = new List;
+            current = current->next;
+        }
+        current->val = nums[i];
+    }
+
+    // delete element
+    //l = del(l, 0);
+    del2(l, 0); 
+
+
+    // print
+    current = l;
+    while (current != NULL)
+    {
+        std::cout << current->val << " ";
+        current = current->next;
+    }
+    std::cout << "\n";
+
+    // delete List
+    current = l;
+    List* tmp;
+    while (current != NULL)
+    {
+        tmp = current;
+        std::cout << current->val << "bye ";
+        current = current->next;
+        delete tmp;
+    }
+    std::cout << "\n";
+
+    return 0;
+}
---
+++ point.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+//call by pointer
+void set1(int *a) {
+	*a = 1;
+}
+//call by referance
+void set2(int& a) {
+	a = 2;
+}
+void set3(int *a) {
+	*a = 2;
+}
+int main() {
+	int a = 5;
+	//call by pointer
+	set1(&a);
+	std::cout << a << "\n";
+	//call by referance ***only for c++
+	set2(a);
+	std::cout << a << "\n";
+
+	//------//
+	int *b;
+	int c = 10;
+	b = &c;
+
+	set3(b);
+	std::cout << *b;
+	return 0;
+}
